05d7921e0238cea1dba5f1257b9d312d536d128e
sandalhathat <sandalhathat@gmail.com>
2023-09-07 17:50:36 -0500

    changing it to one argument. it's assumed that the inputted string contains the total amount of space

--- esm.py
+++ esm.py
@@ -1,41 +1,34 @@
 import sys
 
 
-def even_space_text(input_text, total_space):
+def even_space_text(input_text):
+    # Calculate the total space based on the length of the input string
+    total_space = len(input_text)
+
     # Split the input text into terms
     terms = input_text.split()
 
-    # Calculate the number of terms
-    num_terms = len(terms)
-
-    # Calculate the remaining space after placing the terms
-    remaining_space = total_space - len(input_text) + num_terms - 1
-
-    # Calculate the equal spacing between terms
-    if num_terms > 1:
-        equal_spacing = remaining_space // (num_terms - 1)
-    else:
-        equal_spacing = 0
+    # Calculate the space between each term
+    space_between_terms = total_space // (len(terms) - 1)
 
     # Initialize the result string
     result = ""
 
-    # Add the first term
-    result += terms[0]
+    # Iterate through the terms and add them with spaces
+    for term in terms[:-1]:
+        result += term + " " * space_between_terms
 
-    # Add equal spacing and subsequent terms
-    for i in range(1, num_terms):
-        result += " " * equal_spacing + terms[i]
+    # Add the last term without extra space
+    result += terms[-1]
 
     return result
 
 
 if __name__ == "__main__":
-    # Check for the correct number of command-line arguments
-    if len(sys.argv) != 3:
-        print("Usage: python even_space_mcface.py [input_text] [total_space]")
-    else:
-        input_text = sys.argv[1]
-        total_space = int(sys.argv[2])
-        result_text = even_space_text(input_text, total_space)
-        print(result_text)
+    if len(sys.argv) != 2:
+        print("Usage: python even_space_mcface.py <input_text>")
+        sys.exit(1)
+
+    input_text = sys.argv[1]
+    output_text = even_space_text(input_text)
+    print(output_text)
