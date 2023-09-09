f08c65f5469ec9c6fdbc310294992b0a8d6a35a5
sandalhathat <sandalhathat@gmail.com>
2023-09-09 14:19:35 -0500

    almost perfect, at least output string is correct now

--- esm.py
+++ esm.py
@@ -1,51 +1,43 @@
-import os
 from datetime import datetime
 
 def evenly_space_text():
     # Get user input for the text
     input_text = input("Enter the text: ")
 
-    # Count the total number of spaces in the input text
-    total_spaces = input_text.count(' ')
-
     # Split the input text into words
     words = input_text.split()
 
-    # Calculate the total length of all words
-    total_length = sum(len(word) for word in words)
+    # Calculate the total number of spaces needed to evenly distribute
+    total_spaces = len(words) - 1
 
-    # Calculate the desired space length
-    space_length = total_spaces // (len(words) - 1) if len(words) > 1 else 0
+    # Calculate the number of spaces to place between terms
+    spaces_between_terms = len(input_text) - len("".join(words))
+
+    # Calculate the total length of the output string
+    total_length = len(input_text) + spaces_between_terms
 
     # Initialize the result
     result = ""
 
-    # Initialize variables to track total space and spaces placed
-    total_space_placed = 0
-    spaces_placed = 0
-
     # Iterate through words and add spaces
-    for word in words:
+    for i, word in enumerate(words):
         result += word
-        if word != words[-1]:
-            spaces_to_add = space_length + spaces_placed
-            result += " " * spaces_to_add
-            total_space_placed += spaces_to_add
-            spaces_placed = space_length
+        if i < len(words) - 1:
+            spaces_to_add = " " * (spaces_between_terms // total_spaces)
+            result += spaces_to_add
 
     # Generate a timestamp for the file name
     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
-    # Create a unique file name with the timestamp
-    file_name = f"evenly_spaced_text_{timestamp}.txt"
+    # Create or append to the "output.txt" file
+    with open("output.txt", "a") as file:
+        file.write(f"Total spaces in input text: {total_length}\n")
+        file.write(f"Spaces placed between terms: {spaces_between_terms}\n")
+        file.write(f"Evenly spaced text: \"{result}\"\n\n")
 
-    # Save the result to a text file
-    with open(file_name, "w") as file:
-        file.write(result)
-
-    print(f"Evenly spaced text saved to {file_name}")
-    print(f"Total spaces in input text: {total_spaces}")
-    print(f"Spaces placed between terms: {total_space_placed}")
+    print(f"Total spaces in input text: {total_length}")
+    print(f"Spaces placed between terms: {spaces_between_terms}")
+    print(f"Evenly spaced text saved to output.txt")
 
 
 if __name__ == "__main__":
