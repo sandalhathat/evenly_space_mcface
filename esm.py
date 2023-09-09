c437b08c45c09d9aa062b3459564d7b91c9a2acb
sandalhathat <sandalhathat@gmail.com>
2023-09-09 12:09:32 -0500

    adjusting things. I want it to print out a few things, for debugging purposes. For one, to print out the total amount of space it thinks is there, and secondly, to print out how much space it thinks it has correctly placed between terms

--- esm.py
+++ esm.py
@@ -5,35 +5,47 @@ def evenly_space_text():
     # Get user input for the text
     input_text = input("Enter the text: ")
 
+    # Count the total number of spaces in the input text
+    total_spaces = input_text.count(' ')
+
     # Split the input text into words
     words = input_text.split()
 
-    # Calculate the total length of all words and spaces
-    total_length = len(input_text)
+    # Calculate the total length of all words
+    total_length = sum(len(word) for word in words)
 
-    # Calculate the desired space between words
-    space_length = total_length // (len(words) - 1)
+    # Calculate the desired space length
+    space_length = total_spaces // (len(words) - 1) if len(words) > 1 else 0
 
     # Initialize the result
     result = ""
+
+    # Initialize variables to track total space and spaces placed
+    total_space_placed = 0
+    spaces_placed = 0
 
     # Iterate through words and add spaces
     for word in words:
         result += word
         if word != words[-1]:
-            result += " " * space_length
+            spaces_to_add = space_length + spaces_placed
+            result += " " * spaces_to_add
+            total_space_placed += spaces_to_add
+            spaces_placed = space_length
 
     # Generate a timestamp for the file name
     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
     # Create a unique file name with the timestamp
-    file_name = f"esm_{timestamp}.txt"
+    file_name = f"evenly_spaced_text_{timestamp}.txt"
 
     # Save the result to a text file
     with open(file_name, "w") as file:
         file.write(result)
 
     print(f"Evenly spaced text saved to {file_name}")
+    print(f"Total spaces in input text: {total_spaces}")
+    print(f"Spaces placed between terms: {total_space_placed}")
 
 
 if __name__ == "__main__":
